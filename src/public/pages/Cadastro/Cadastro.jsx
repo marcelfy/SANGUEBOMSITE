b5d90433dbfced7ffc46bac0ab39bb46110cdc36
Marcel Ferreira Yassumoto <marcelferreirayassumoto2002@hotmail.com>
2022-05-24 22:02:33 -0400

    cadastro page

--- src/public/pages/Cadastro/Cadastro.jsx
+++ src/public/pages/Cadastro/Cadastro.jsx
@@ -1,9 +1,54 @@
 import Style from './Cadastro.module.css'
+import { Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css'
+
 
 const Cadastro = () => {
-    return(
-        <h1>Cadastro</h1>
-    )
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+    
+      return (
+        <fieldset className={Style.form}>
+            <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            >
+            <Form.Item
+                label="Nome"
+                name="nome"
+                rules={[{ required: true, message: 'Campo nome é obrigatório' }]}
+            >
+                <Input className={Style.input}/>
+            </Form.Item>
+        
+            <Form.Item
+                label="Senha"
+                name="senha"
+                rules={[{ required: true, message: 'O campo senha é obrigatório' }]}
+            >
+                <Input.Password className={Style.input}/>
+            </Form.Item>
+        
+            
+        
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit" className={Style.input}>
+                Cadastrar
+                </Button>
+            </Form.Item>
+            </Form>
+        </fieldset>
+      )
 }
 
 export default Cadastro
