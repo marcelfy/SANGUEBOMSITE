f41ebb2e25713bf3baca1a168a0e7396da5c4153
Marcel Ferreira Yassumoto <marcel10042002@gmail.com>
2023-05-11 18:56:19 -0400

    remove api

--- api/routes/PerfilRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import PerfilController from '../controllers/PerfilController.js';
-import { verifyJWT } from '../Middleware/verifyJWT.js';
-
-const PerfilRoute  = express.Router();
-
-PerfilRoute.get("/perfil" , PerfilController.get);
-
-export default PerfilRoute;
